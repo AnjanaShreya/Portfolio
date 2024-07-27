69dbb54a8aa5a163a6a771c41a9fbb9a83bf24aa
AnjanaShreya <chslash01@yahoo.com>
2024-07-27 09:00:06 +0530

    commit

---
+++ assets/js/meyawo.js
@@ -0,0 +1,60 @@
+/*!
+=========================================================
+* Meyawo Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+// navbar toggle
+$('#nav-toggle').click(function(){
+    $(this).toggleClass('is-active')
+    $('ul.nav').toggleClass('show');
+});
+
+
+document.getElementById('downloadBtn').addEventListener('click', function () {
+    // Create a link element
+    const link = document.createElement('a');
+    
+    // Set the href to the path of the file to download
+    // Replace 'path/to/your/file.pdf' with the actual path to your file
+    link.href = './Ch S Anjana Shreya-fullstack.pdf';
+    
+    // Set the download attribute with a file name
+    link.download = 'Anjana Shreya - CV.pdf';
+    
+    // Append the link to the body (required for Firefox)
+    document.body.appendChild(link);
+    
+    // Programmatically click the link to trigger the download
+    link.click();
+    
+    // Remove the link from the document
+    document.body.removeChild(link);
+});
